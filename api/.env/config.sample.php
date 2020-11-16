fd8cfeea9fdd8bf7c7bf49c0284689e07639161d
Dieunelson Dorcelus <43044653+dd701116@users.noreply.github.com>
2020-11-16 06:03:32 +0100

    Create config.sample.php

---
+++ api/.env/config.sample.php
@@ -0,0 +1,14 @@
+<?php
+
+
+define('AUTH_DB_HOST', '');
+define('AUTH_DB_PORT', '');
+define('AUTH_DB_USER', '');
+define('AUTH_DB_PASS', '');
+define('AUTH_DB_NAME', '');
+
+define('DB_SOURCE_TABLE', '');
+define('DB_CHANNEL_TABLE', '');
+
+
+
